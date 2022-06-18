d088fc4886b71afe1f26b2a52ec8963abd6a7b83
Niels <niels.koster@portavita.nl>
2022-06-18 18:32:40 +0200

    Add week grid

--- src/components/ContainerWeek.js
+++ src/components/ContainerWeek.js
@@ -1,12 +1,14 @@
 import './ContainerWeek.css'
 import {WeekHeader} from './WeekHeader'
 import {DayEvents} from './DayEvents'
+import {WeekGrid} from './WeekGrid'
 
 export function ContainerWeek() {
   return (
     <div className='ContainerWeek'>
       <WeekHeader />
       <DayEvents />
+      <WeekGrid />
     </div>
   )
 }
---
+++ src/components/WeekGrid.js
@@ -0,0 +1,23 @@
+import './WeekGrid.css'
+
+export function WeekGrid() {
+  return (
+    <div className='WeekScroller'>
+      <div className='WeekGrid'>
+        {buildGrid()}
+      </div>
+    </div>
+  )
+}
+
+function buildGrid() {
+  const grid = []
+  for (let hour = 0; hour < 24; hour++) {
+    for (let week = 0; week < 7; week++) {
+      // const text = document.createTextNode(`${hour.toString().padStart(2, '0')}:00`)
+      // item.classList.add('item-hour')
+      grid.push(<div key={`${hour} ${week}`} className='ItemDayEvents ItemHour'>{`${hour.toString().padStart(2, '0')}:00`}</div>)
+    }
+  }
+  return grid
+}
