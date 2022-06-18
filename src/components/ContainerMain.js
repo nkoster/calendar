6da61e8bb87c27854f0831f57c6dc12e831c191c
Niels <niels.koster@portavita.nl>
2022-06-18 15:51:50 +0200

    Add container for week grid

--- src/components/ContainerMain.js
+++ src/components/ContainerMain.js
@@ -1,11 +1,13 @@
 import {ContainerMonth} from './ContainerMonth'
 import {ContainerFilters} from './ContainerFilters'
+import {ContainerWeek} from './ContainerWeek'
 
 export function ContainerMain() {
   return (
     <div className='ContainerMain'>
       <ContainerMonth />
       <ContainerFilters />
+      <ContainerWeek />
     </div>
   )
 }
