1030dd73659e306f01ed68dfd2c8e041c8d9d2b2
Niels <niels.koster@portavita.nl>
2022-06-21 17:59:47 +0200

    Check for today + Show date/time in grid

--- src/components/WeekGridItem.js
+++ src/components/WeekGridItem.js
@@ -1,13 +1,25 @@
 import './WeekGridItem.css'
 import {useEffect, useRef, useState} from 'react'
+import {getMonthGrid} from '../util/getMonthGrid'
 
-export function WeekGridItem({week, hour}) {
+export function WeekGridItem({timestamp, week, hour}) {
+
+  const monthTable = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const grid = getMonthGrid(timestamp)
+  const weekDays = grid.filter(week => {
+    return week.includes(new Date(timestamp).getDate())
+  })
+
+  const year = new Date(timestamp).getFullYear()
+  const month = new Date(timestamp).getMonth()
 
   const d = new Date()
   const dayOfWeek = d.getDay()
   const [currentMinutes, setCurrentMinutes] = useState(d.getMinutes())
   const [currentHour, setCurrentHour] = useState(d.getHours())
   const focus = useRef()
+
+  const isToday = new Date().toDateString() === new Date(timestamp).toDateString()
 
   useEffect(() => {
     setInterval(() => {
@@ -21,13 +33,13 @@ export function WeekGridItem({week, hour}) {
   }, [])
 
   return (
-    <div className={`ItemDayEvents ItemHour ${
-      dayOfWeek === week ? 'ItemCurrent' : ''
+    <div className={`ItemHour ${
+      dayOfWeek === week && isToday ? 'ItemCurrent' : ''
     } ${
-      dayOfWeek === week && currentHour === hour ? 'ItemCurrenTime' : ''
-    }`}>
+      dayOfWeek === week && currentHour === hour && isToday ? 'ItemCurrenTime' : ''
+    }`} title={`${year}, ${monthTable[month]} ${weekDays[0][week]}, ${hour.toString().padStart(2, '0')}:00`}>
       {`${hour.toString().padStart(2, '0')}:00`}
-      {dayOfWeek === week && currentHour === hour ?
+      {dayOfWeek === week && currentHour === hour && isToday?
         <div ref={focus} style={{
           position: 'absolute',
           left: '0',
