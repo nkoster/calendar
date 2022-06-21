d6604ba41a81668b336fe0d0b06fe4924bfa268d
Niels <niels.koster@portavita.nl>
2022-06-21 17:31:43 +0200

    Check for today

--- src/components/DayEvents.js
+++ src/components/DayEvents.js
@@ -1,24 +1,25 @@
 import './DayEvents.css'
 
-export function DayEvents() {
+export function DayEvents({timestamp}) {
 
   return (
     <div className={`DayEventsScroller`}>
       <div className={`DayEventsGrid`}>
-        {buildGrid()}
+        {buildGrid(timestamp)}
       </div>
     </div>
   )
 }
 
-function buildGrid() {
+function buildGrid(timestamp) {
   const d = new Date()
   const dayOfWeek = d.getDay()
   const grid = []
+  const isToday = new Date().toDateString() === new Date(timestamp).toDateString()
   for (let day = 0; day < 20; day++) {
     for (let week = 0; week < 7; week++) {
       grid.push(
-        <div key={`${day} ${week}`} className={`DayEventsGridItem ${dayOfWeek === week ? 'DayCurrent' : ''}`}>
+        <div key={`${day} ${week}`} className={`DayEventsGridItem ${dayOfWeek === week && isToday? 'DayCurrent' : ''}`}>
 
         </div>
       )
