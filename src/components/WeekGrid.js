61678044952dde537cb3b46141f2ad9e937dc313
Niels <niels.koster@portavita.nl>
2022-06-21 17:59:03 +0200

    Work with timestamp

--- src/components/WeekGrid.js
+++ src/components/WeekGrid.js
@@ -1,26 +1,24 @@
 import './WeekGrid.css'
 import {WeekGridItem} from './WeekGridItem'
 
-export function WeekGrid(date) {
+export function WeekGrid({timestamp}) {
   return (
     <div className='WeekScroller'>
       <div className='WeekGrid'>
-        {buildGrid(date)}
+        {buildGrid(timestamp)}
       </div>
     </div>
   )
 }
 
-function buildGrid(date) {
+function buildGrid(timestamp) {
   const grid = []
   let count = 0
-  // const d = new Date(date.date)
-  // console.log(d)
   for (let hour = 0; hour < 24; hour++) {
     for (let week = 0; week < 7; week++) {
       count++
       grid.push(
-        <WeekGridItem key={`${count}`} date={date} week={week} hour={hour}/>
+        <WeekGridItem key={`${count}`} timestamp={timestamp} week={week} hour={hour}/>
       )
     }
   }
