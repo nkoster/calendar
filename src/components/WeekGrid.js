90e81e04963e8b0dbcdfaedf311d05bb133a1206
Niels <niels.koster@portavita.nl>
2022-06-23 21:22:57 +0200

    Renew when today flips

--- src/components/WeekGrid.js
+++ src/components/WeekGrid.js
@@ -1,24 +1,35 @@
 import './WeekGrid.css'
 import {WeekGridItem} from './WeekGridItem'
+import {useEffect, useState} from 'react'
 
-export function WeekGrid({timestamp}) {
+export function WeekGrid() {
+
+  const today = new Date().getDay()
+  const [showDay, setShowDay] = useState(today)
+
+  useEffect(() => {
+    setInterval(() => {
+      setShowDay(new Date().getDay())
+    }, 2000)
+  }, [])
+
   return (
     <div className='WeekScroller'>
       <div className='WeekGrid'>
-        {buildGrid(timestamp)}
+        {buildGrid(showDay)}
       </div>
     </div>
   )
 }
 
-function buildGrid(timestamp) {
+function buildGrid(showDay) {
   const grid = []
   let count = 0
   for (let hour = 0; hour < 24; hour++) {
     for (let week = 0; week < 7; week++) {
       count++
       grid.push(
-        <WeekGridItem key={`${count}`} timestamp={timestamp} week={week} hour={hour}/>
+        <WeekGridItem key={`${count}`} showDay={showDay} week={week} hour={hour}/>
       )
     }
   }
