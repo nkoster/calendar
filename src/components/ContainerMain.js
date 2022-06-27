f61792b23b94ad11e7a5b20eefe8704b0a65a172
Niels <niels.koster@portavita.nl>
2022-06-28 01:02:45 +0200

    Set timestamp to today when week is current week

--- src/components/ContainerMain.js
+++ src/components/ContainerMain.js
@@ -2,8 +2,22 @@ import {ContainerMonth} from './ContainerMonth'
 import {ContainerFilters} from './ContainerFilters'
 import {ContainerWeek} from './ContainerWeek'
 import {ContainerHeader} from './ContainerHeader'
+import {useEffect} from 'react'
+import {State, StateDispatch, updateTimestamp} from '../state'
+import {getWeekNumber} from '../util'
 
 export function ContainerMain() {
+
+  const weekNumber = getWeekNumber(new Date().toString())
+  const {timestamp} = State()
+  const dispatch = StateDispatch()
+
+  useEffect(() => {
+    if (weekNumber === getWeekNumber(timestamp)) {
+      updateTimestamp(dispatch, new Date().toString()).catch(err => console.log(err))
+    }
+  }, [timestamp, dispatch, weekNumber])
+
   return (
     <div className='ContainerMain'>
       <ContainerHeader />
@@ -12,4 +26,5 @@ export function ContainerMain() {
       <ContainerWeek />
     </div>
   )
+
 }
